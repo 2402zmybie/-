0626f51a9f3f7ad2bf8d2a194af1e7706eb46c0b
Lc <641229781@qq.com>
2020-09-14 14:49:18 +0800

    小纸条下拉刷新

---
+++ components/paper/paper-list.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="paper-list u-f-ac">
+		<image :src="item.userpic" mode="widthFix" lazy-load></image>
+		<view>
+			<view class="u-f-ac u-f-jsb">
+				{{ item.username }}
+				<view>{{ item.time }}</view>
+			</view>
+			<view class="u-f-ac u-f-jsb">
+				{{ item.data }}
+				<template v-if="item.noreadnum != 0">
+					<uni-badge :text="item.noreadnum + ''" type="error"></uni-badge>
+				</template>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniBadge from '../../components/uni-badge/uni-badge.vue'
+	export default {
+		components: {
+			uniBadge
+		},
+		props: {
+			item:Object
+		}
+	}
+</script>
+
+<style lang="scss">
+.paper-list {
+	border-bottom: 1upx solid #EEEEEE;
+	padding: 20upx 0;
+	image {
+		width: 100upx;
+		height: 100upx;
+		border-radius: 100%;
+		margin-right: 20upx;
+		flex-shrink: 0;
+	}
+	>view {
+		flex: 1;
+		>view:nth-child(1) {
+			font-size: 35upx;
+			>view {
+			   color: #CBCBCB;
+			}
+		}
+		>view:nth-child(2) {
+			color: #999999;
+		}
+	}
+}
+</style>
--- pages/paper/paper.vue
+++ pages/paper/paper.vue
@@ -1,22 +1,161 @@
 <template>
-	<view>
-		小纸条通讯
+	<view class="body">
+		<!-- 小纸条列表 -->
+		<block v-for="(item,index) in list" :key="index">
+			<paper-list :item="item"></paper-list>
+		</block>
+		
+		
 	</view>
 </template>
 
 <script>
+	import paperList from '../../components/paper/paper-list.vue'
 	export default {
+		components:{
+			paperList
+		},
 		data() {
 			return {
-				
+				show:false,//是否显示菜单
+				loadtext:"上拉加载更多",
+				list:[
+						{
+							userpic:"../../static/demo/userpic/1.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:2
+						},
+						{
+							userpic:"../../static/demo/userpic/2.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/3.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/4.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:11
+						},
+						{
+							userpic:"../../static/demo/userpic/5.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:2
+						},
+						{
+							userpic:"../../static/demo/userpic/6.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/7.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/8.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:11
+						}
+					]
 			}
 		},
+		onPullDownRefresh() {
+			this.getData()
+		},
 		methods: {
-			
-		}
+			getData() {
+				setTimeout(() => {
+					let arr = [
+						{
+							userpic:"../../static/demo/userpic/11.jpg",
+							username:"昵称111",
+							time:"10:21",
+							data:"我是信息111",
+							noreadnum:2
+						},
+						{
+							userpic:"../../static/demo/userpic/2.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/3.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/4.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:11
+						},
+						{
+							userpic:"../../static/demo/userpic/5.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:2
+						},
+						{
+							userpic:"../../static/demo/userpic/6.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/7.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:0
+						},
+						{
+							userpic:"../../static/demo/userpic/8.jpg",
+							username:"昵称",
+							time:"10:21",
+							data:"我是信息",
+							noreadnum:11
+						}
+					]
+					this.list = arr
+					uni.stopPullDownRefresh()
+					
+				}, 2000);
+			}
+		},
+		
 	}
 </script>
 
-<style>
+<style lang="scss">
+.body {
+	padding: 0 20upx;
+}
 
 </style>
