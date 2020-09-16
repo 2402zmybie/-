9b5c3a737afcea91633c9d07b3ef131c59153509
Lc <641229781@qq.com>
2020-09-16 10:10:18 +0800

    聊天列表页面双方消息界面完成

--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -1,12 +1,23 @@
 <template>
 	<view>
 		<!-- 聊天列表 -->
-		<view class="user-chat-list u-f">
-			<image src="../../static/demo/userpic/1.jpg" mode="widthFix" lazy-load></image>
-			<view class="user-chat-list-body">
-				<text>你好亚</text>
+		<block v-for="(item,index) in list" :key="index">
+			<view class="user-chat-time u-f-ajc">{{ item.time }}</view>
+			<view class="user-chat-list u-f " :class="{'user-chat-me': item.isme}">
+				<image v-if="!item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
+				<view class="user-chat-list-body">
+					<!-- 文字 -->
+					<text v-if="item.type == 'text'">{{ item.data }}</text>
+					<!-- 图片 -->
+					<image v-if="item.type == 'img'" :src="item.data" mode="widthFix"></image>
+				</view>
+				
+				<!-- 我发送的消息,头像在右边 -->
+				<image v-if="item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
+				
 			</view>
-		</view>
+		</block>
+		
 		
 		<!-- 输入框 -->
 		<user-chat-bottom @submit="submit"></user-chat-bottom>
@@ -21,7 +32,43 @@
 		},
 		data() {
 			return {
-				
+				list:[
+					{
+						isme:false,
+						userpic:"../../static/demo/userpic/11.jpg",
+						type:"text",
+						data:"哈哈哈",
+						time:"1555146412"
+					},
+					{
+						isme:true,
+						userpic:"../../static/demo/userpic/10.jpg",
+						type:"img",
+						data:"../../static/demo/3.jpg",
+						time:"1555146414",
+					},
+					{
+						isme:false,
+						userpic:"../../static/demo/userpic/11.jpg",
+						type:"text",
+						data:"哈哈哈",
+						time:"1555146412"
+					},
+					{
+						isme:true,
+						userpic:"../../static/demo/userpic/10.jpg",
+						type:"img",
+						data:"../../static/demo/4.jpg",
+						time:"1555146414",
+					},
+					{
+						isme:true,
+						userpic:"../../static/demo/userpic/10.jpg",
+						type:"img",
+						data:"../../static/demo/4.jpg",
+						time:"1555146414",
+					}
+				]
 			}
 		},
 		methods: {
@@ -33,8 +80,14 @@
 </script>
 
 <style lang="scss">
+.user-chat-time {
+	padding: 50upx 0;
+	color: #a2a2a2;
+	font-size: 24upx;
+}
 .user-chat-list {
-	image {
+	padding: 20upx;
+	>image {
 		width: 100upx;
 		height: 100upx;
 		border-radius: 100%;
@@ -58,6 +111,22 @@
 			height: 0;
 			border: 16upx solid #F4F4F4;
 			border-color: transparent #F4F4F4 transparent transparent;
+		}
+		image {
+			max-width: 200upx;
+			max-height: 150upx;
+		}
+	}
+}
+.user-chat-me {
+	justify-content: flex-end;
+	.user-chat-list-body {
+		margin-right: 20upx;
+		margin-left: 100upx;
+		:after {
+			left: auto;
+			right: -30upx;
+			border-color: transparent  transparent transparent #F4F4F4;
 		}
 	}
 }
